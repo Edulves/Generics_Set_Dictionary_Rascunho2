e7600fb1573e089523d733ce3a11febc025fe903
Eduardo Alves <eduardo.lves@outlook.com>
2023-08-12 08:35:57 -0300

    First commit

---
+++ Generics_Set_Dictionary_Rascunho2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Services;
+
+List<int> list = new List<int>();
+
+Console.Write("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++) {
+    int x = int.Parse(Console.ReadLine());
+    list.Add(x);
+}
+
+CalculationService calculationService = new CalculationService();
+
+int max = calculationService.Max(list);
+
+Console.WriteLine("Max: ");
+Console.WriteLine(max);
